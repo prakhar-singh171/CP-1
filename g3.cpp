e7f1c0d410edbb96c90fb040eafef17a97115f65
prkr15 <prakkhar15@gmail.com>
2025-01-06 20:46:22 +0530

    first commit

---
+++ g3.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     #ifndef ONLINE_JUDGE
+        freopen("input1.txt", "r", stdin);
+        freopen("input2.txt", "w", stdout);
+        freopen("error1.txt", "w", stderr);
+    #endif
+
+    int test = 1;
+    cin >> test;
+    while(test--) cout<<"fdf";
+}
